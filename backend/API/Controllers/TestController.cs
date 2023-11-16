1cdd834641bbf03ef6998527f9d06bb2da26c8cf
Máté Rák <rakmate9812@gmail.com>
2023-11-16 16:51:49 +0100

    Add project files.

---
+++ backend/API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : Controller
+    {
+        [HttpGet("test")]
+        public string GetTestData()
+        {
+            return "ok";
+        }
+    }
+}
