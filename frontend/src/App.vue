24d6b87e580a22cea7918128703ea02a30ad0688
Máté Rák <rakmate9812@gmail.com>
2023-11-18 15:47:39 +0100

    fixing prettier fomrmatting errors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/about">Menu</router-link>
+      <router-link to="/menu">Menu</router-link>
     </nav>
     <router-view />
   </div>
